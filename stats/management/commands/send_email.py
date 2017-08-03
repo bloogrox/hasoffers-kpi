8cf7808d979108a117e721384c4c4ad246e83620
lericdev <leric.dev@hotmail.com>
2017-08-04 04:15:19 +0800

    fixed flake8 warning again.

--- stats/management/commands/send_email.py
+++ stats/management/commands/send_email.py
@@ -1,5 +1,5 @@
 import sendgrid
-from sendgrid.helpers.mail import *
+from sendgrid.helpers.mail import *  # noqa
 
 from django.core.management.base import BaseCommand
 from django.conf import settings
