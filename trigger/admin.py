098f764b53a79f173a9ee41487e9ca436f4f75a2
aslan <bloogrox@gmail.com>
2017-08-12 22:05:59 +0300

    add TriggerCheck to admin

--- trigger/admin.py
+++ trigger/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 
-from trigger.models import Trigger
+from trigger.models import Trigger, TriggerCheck
 
 
 @admin.register(Trigger)
 class TriggerAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'metric', 'operator', 'active',)
     list_filter = ('metric', 'active',)
+
+
+@admin.register(TriggerCheck)
+class TriggerCheckAdmin(admin.ModelAdmin):
+    list_display = ('id', 'created_at', 'trigger', 'offer_id',
+                    'affiliate_id', 'status',)
+    list_filter = ('trigger', 'status', 'offer_id', 'affiliate_id')
