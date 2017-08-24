047b490a1618021ad7d88f1d6c6255a496bfd6a4
aslan <bloogrox@gmail.com>
2017-08-25 02:49:16 +0300

    add models Notification, NotificationReceiverType; add relation Action.notifications

--- actions/admin.py
+++ actions/admin.py
@@ -1,18 +1,43 @@
 from django.contrib import admin
-from actions.models import Action, Operation
+from actions.models import (Action, Operation, Notification,
+                            NotificationReceiverType)
 
 
 class OperationInline(admin.TabularInline):
     model = Operation.actions.through
+    extra = 0
+    verbose_name = 'Operation'
+    verbose_name_plural = 'Operations'
+
+
+class NotificationInline(admin.StackedInline):
+    model = Notification
+    extra = 0
 
 
 @admin.register(Action)
 class ActionAdmin(admin.ModelAdmin):
     list_display = ('id', 'trigger', 'trigger_status')
-    inlines = (OperationInline,)
+    inlines = (
+        NotificationInline,
+        OperationInline,
+    )
     exclude = ('operations',)
+    fieldsets = (
+        ('Conditions', {'fields': ('trigger', 'trigger_status',)}),
+    )
 
 
 @admin.register(Operation)
 class OperationAdmin(admin.ModelAdmin):
     list_display = ('id', 'key', 'title')
+
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'subject',)
+
+
+@admin.register(NotificationReceiverType)
+class NotificationReceiverTypeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
--- actions/models.py
+++ actions/models.py
@@ -15,3 +15,21 @@ class Action(models.Model):
     trigger_status = models.CharField(max_length=32,
                                       choices=TriggerCheck.STATUSES)
     operations = models.ManyToManyField(Operation, related_name='actions')
+
+
+class NotificationReceiverType(models.Model):
+    name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.name
+
+
+class Notification(models.Model):
+    subject = models.CharField(max_length=256)
+    message = models.TextField()
+    # help_text='Available macroses are: {METRIC.affiliate_id}'
+    action = models.ForeignKey(Action, related_name='notifications')
+    receivers = models.ManyToManyField(NotificationReceiverType)
+
+    def __str__(self):
+        return self.subject
