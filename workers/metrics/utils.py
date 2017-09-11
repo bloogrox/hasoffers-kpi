cc3ae1652a7f41f1f26af69e5e81651c21238c55
aslan <bloogrox@gmail.com>
2017-09-11 21:10:58 +0300

    return 0 if offer doesnt exist

--- workers/metrics/utils.py
+++ workers/metrics/utils.py
@@ -11,10 +11,9 @@ def offer_exists_and_monitoring_true(offer_id):
 
 def get_offer_min_clicks(offer_id):
     from stats.models import Offer
-    from stats.signals import offer_does_not_exist
 
     try:
         offer = Offer.objects.get(pk=offer_id)
         return offer.min_clicks
     except Offer.DoesNotExist:
-        offer_does_not_exist.send(sender=None, offer_id=offer_id)
+        return 0
